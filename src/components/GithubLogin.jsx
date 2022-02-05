62517ccd7adfa0c20390ae3714fd18c186d2b933
Enger Then <35516612+ethen001@users.noreply.github.com>
2022-02-04 20:31:04 -0500

    added api call for github

--- src/components/GithubLogin.jsx
+++ src/components/GithubLogin.jsx
@@ -1,19 +1,22 @@
 import React, { useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import getVar from '../config/envConfig';
+import { authenticateGitHubUser } from '../services/userRequests';
 
 
 export default function GithubLogin() {
   const authUrl = getVar('GITHUB_AUTH_URL');
-  const clientId = getVar('GITHUB_CLIENT_ID')
-  const clientSecret = getVar('GITHUB_CLIENT_SECRET')
+  const clientId = getVar('GITHUB_CLIENT_ID');
+  const clientSecret = getVar('GITHUB_CLIENT_SECRET');
   const urlParams = new URLSearchParams(window.location.search);
   const code = urlParams.get('code');
   
   useEffect(() => {
     if (code) {
-      // TODO: logic to send code to backend
-      console.log(code)
+      authenticateGitHubUser(code)
+      .then((data) => {
+        console.log(data)
+      })
     }
   }, [code])
 
