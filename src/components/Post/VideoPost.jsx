920859ba8bed5b1cf9bb872b270ad6d0a6a5d7d0
Enger Then <35516612+ethen001@users.noreply.github.com>
2022-04-14 14:36:31 -0400

    Added video streaming route

--- src/components/Post/VideoPost.jsx
+++ src/components/Post/VideoPost.jsx
@@ -1,26 +1,28 @@
+import React from "react";
 import { Box } from "@mui/material";
-import React, { useState } from "react";
+import { CircularProgress } from "@mui/material";
 import { useQuery } from "react-query";
 import { useSelector } from "react-redux";
-import { CircularProgress } from "@mui/material";
-import fetchMediaFile from "../../utils/mediaStorage/fetchMediaFile";
+import getVar from "../../config/envConfig";
+import getFileDownloadUrl from '../../utils/mediaStorage/getFileDownloadUrl';
 
 export default function VideoPost({ filePath }) {
   const userToken = useSelector((state) => state.userToken);
-  const { isLoading, error, data } = useQuery("mediaBlob", () =>
-    fetchMediaFile(userToken, filePath)
-  );
-  if (isLoading) {
-    return (
-      <Box>
-        <CircularProgress />
-      </Box>
-    )
-  }
+  const { isLoading, data} = useQuery("storageToken", () => getFileDownloadUrl(userToken, filePath));
+if (isLoading) {
+  return (
+    <Box>
+      <CircularProgress />
+    </Box>
+  )
+}
   return (
     <Box>
       <video controls width="100%" height="100%">
-        <source src={data?.mediaBlobUrl} type="video/mp4" />
+        <source
+          src={data.mediaDownloadUrl}
+          type="video/mp4"
+        />
       </video>
     </Box>
   );
