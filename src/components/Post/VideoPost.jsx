2aa0c56b3f418ba158ab338fc4f593acc66a2dfb
Enger Then <35516612+ethen001@users.noreply.github.com>
2022-04-17 13:46:53 -0400

    Added media video delete functionality

--- src/components/Post/VideoPost.jsx
+++ src/components/Post/VideoPost.jsx
@@ -3,26 +3,24 @@ import { Box } from "@mui/material";
 import { CircularProgress } from "@mui/material";
 import { useQuery } from "react-query";
 import { useSelector } from "react-redux";
-import getVar from "../../config/envConfig";
-import getFileDownloadUrl from '../../utils/mediaStorage/getFileDownloadUrl';
+import getFileDownloadUrl from "../../utils/mediaStorage/getFileDownloadUrl";
 
 export default function VideoPost({ filePath }) {
   const userToken = useSelector((state) => state.userToken);
-  const { isLoading, data} = useQuery("storageToken", () => getFileDownloadUrl(userToken, filePath));
-if (isLoading) {
-  return (
-    <Box>
-      <CircularProgress />
-    </Box>
-  )
-}
+  const { isLoading, data } = useQuery("storageToken", () =>
+    getFileDownloadUrl(userToken, filePath)
+  );
+  if (isLoading) {
+    return (
+      <Box>
+        <CircularProgress />
+      </Box>
+    );
+  }
   return (
     <Box>
       <video controls width="100%" height="100%">
-        <source
-          src={data.mediaDownloadUrl}
-          type="video/mp4"
-        />
+        <source src={data.mediaDownloadUrl} type="video/mp4" />
       </video>
     </Box>
   );
--- src/utils/posts/deletePost.js
+++ src/utils/posts/deletePost.js
@@ -1,13 +1,20 @@
 import axios from "axios";
 import getVar from "../../config/envConfig";
 
-export default async function deletePost(userToken, docId, postId) {
-  if (userToken && docId) {
+export default async function deletePost(userToken, docId, postData) {
+  if (userToken && postData.id) {
     const opts = {
       headers: {
         Authorization: `Bearer ${userToken}`,
       },
     };
+    const { id: postId, postType, mediaFilePath } = postData;
+    if (postType === 'video') {
+      await axios.delete(
+        `${getVar("API_HOST")}/api/storage?filePath=${mediaFilePath}`,
+        opts
+      );
+    }
     const response = await axios.delete(
       `${getVar("API_HOST")}/api/posts/${postId}?doc_id=${docId}`,
       opts
