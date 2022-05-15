4ddbeae928f82db22bdf92695aca17fb5d2d0082
Enger Then <35516612+ethen001@users.noreply.github.com>
2022-05-15 11:50:32 -0400

    made sidabr dynamic

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,44 +1,102 @@
-import React, { useState } from 'react';
-import { Button, Typography, Grid } from '@mui/material';
-import ProjectTreeView from '../ProjectTreeView';
-import styles from './Sidebar.module.css';
-import ProjectForm from '../common/ProjectForm';
-import Loader from '../common/Loader';
-import { useQuery } from 'react-query';
-import { useSelector } from 'react-redux';
-import fetchAllProjects from '../../utils/projects/fetchAllProjects';
-import postProject from '../../utils/projects/postProject';
+import React, { useState } from "react";
+import {
+  Button,
+  Typography,
+  Switch,
+  FormControl,
+  FormControlLabel,
+  Drawer,
+  IconButton,
+} from "@mui/material";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ProjectTreeView from "../ProjectTreeView";
+import styles from "./Sidebar.module.css";
+import ProjectForm from "../common/ProjectForm";
+import Loader from "../common/Loader";
+import { useQuery } from "react-query";
+import { useSelector, useDispatch } from "react-redux";
+import fetchAllProjects from "../../utils/projects/fetchAllProjects";
+import postProject from "../../utils/projects/postProject";
+import { setEditable } from "../../ducks";
+import { DrawerHeader } from "./styles";
+import { drawerWidth } from "../../utils/common/constants";
 
-
-export default function Sidebar() {
+export default function Sidebar({ drawerIsOpened, onClose }) {
   const [opened, setOpened] = useState(false);
-  const userToken = useSelector(state => state.userToken);
-  const { isLoading, data } = useQuery('projects', () => fetchAllProjects(userToken), {
-    enabled: userToken !== undefined || userToken !== '' || userToken !== null,
-  })
+  const { userToken, editable, selectedDocId } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const { isLoading, data } = useQuery(
+    "projects",
+    () => fetchAllProjects(userToken),
+    {
+      enabled:
+        userToken !== undefined || userToken !== "" || userToken !== null,
+    }
+  );
 
   const toggleOpened = () => {
     setOpened(!opened);
   };
 
-  if(isLoading) {
-    return <Loader />
-  }
-  return (
-    <div className={styles.sidebar}>
-      <Button variant="outlined" className={styles.newProjectButton} onClick={toggleOpened}>
-        <Typography component="span">
-          New Project
-        </Typography>
+  const handleChange = (event) => {
+    dispatch(setEditable(event.target.checked));
+  };
+
+  return isLoading ? (
+    <Loader />
+  ) : (
+    <Drawer
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        "& .MuiDrawer-paper": {
+          width: drawerWidth,
+          boxSizing: "border-box",
+          overflowY: "auto",
+          backgroundColor: "#1F5980",
+          color: "#fff",
+          padding: "1%",
+        },
+      }}
+      variant="persistent"
+      anchor="left"
+      open={drawerIsOpened}
+    >
+      <DrawerHeader>
+        <IconButton onClick={onClose}>
+          <ChevronLeftIcon sx={{ color: "#fff" }} />
+        </IconButton>
+      </DrawerHeader>
+      {selectedDocId && (
+        <FormControl>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={editable}
+                onChange={handleChange}
+                color="warning"
+                inputProps={{ "aria-label": "controlled" }}
+              />
+            }
+            label="Edit Document"
+          />
+        </FormControl>
+      )}
+      <Button
+        variant="outlined"
+        className={styles.newProjectButton}
+        onClick={toggleOpened}
+      >
+        <Typography component="span">New Project</Typography>
       </Button>
       <ProjectTreeView projects={data.projects} />
       <ProjectForm
         open={opened}
         onClose={toggleOpened}
-        onMutate={newProject => postProject(userToken, newProject)}
+        onMutate={(newProject) => postProject(userToken, newProject)}
         title="CreateProject"
         buttonText="Create"
       />
-    </div>
+    </Drawer>
   );
 }
---
+++ src/pages/DocIt/DocIt.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Toolbar, IconButton, Box, CssBaseline } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import DocumentContainer from "../../components/DocumentContainer";
+import Sidebar from "../../components/Sidebar";
+import useAuthEffect from "../../hooks/useAuthEffect";
+import { AppBar, Main } from "./styles";
+
+export default function DocIt() {
+  useAuthEffect();
+  const [drawerIsOpened, setDrawerIsOpened] = useState(true);
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        open={drawerIsOpened}
+        color="transparent"
+        variant="outlined"
+      >
+        <Toolbar sx={{ maxHeight: 48 }}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={() => setDrawerIsOpened(true)}
+            edge="start"
+            sx={{
+              marginRight: 5,
+              ...(drawerIsOpened && { display: "none" }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Sidebar
+        drawerIsOpened={drawerIsOpened}
+        onClose={() => setDrawerIsOpened(false)}
+      />
+      <Main open={drawerIsOpened}>
+        <DocumentContainer />
+      </Main>
+    </Box>
+  );
+}
